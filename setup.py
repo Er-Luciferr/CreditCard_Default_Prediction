e0ebf53804e24357b47e3256f8b5727e206a6ca4
Er-Luciferr <mohitsingh2222.ms@gmail.com>
2024-03-17 14:21:26 +0530

    Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="1.0",
+    author="Mohit Singh",
+    author_email='mohitsingh2@yahoo.com',
+    packages=find_packages(),
+    install_requires=[]
+)
