b276bcf731be65cbbe77f9597ae2aea03f369b59
Er-Luciferr <mohitsingh2222.ms@gmail.com>
2024-03-26 22:34:03 +0530

    src\components2

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -59,17 +59,25 @@ class DataTransformation:
 
         try:
             dataframe = pd.read_csv(self.raw_data_dir)
+
             X = dataframe.drop(labels=[TARGET_COLUMN], axis=1)
             y = dataframe[TARGET_COLUMN]
+
             X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.30,random_state=0)
+
             preprocessor = self.get_data_transformer_object()
+
             scaled_X_train = preprocessor.fit_transform(X_train)
             scaled_X_test = preprocessor.transform(X_test)
+
             preprocessor_path = self.DataTransformationConfig.transformed_object_file_path
             os.makedirs(os.path.dirname(preprocessor_path,exist_ok=True))
+            
             self.utils.save_object(file_path=preprocessor_path,obj=preprocessor)
+            
             train_arr = np.c_[scaled_X_train,np.array(X_train)]
             test_arr = np.c_[scaled_X_test,np.array(X_test)]
+            
             return train_arr,test_arr,preprocessor_path
 
 
---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,45 @@
+import os,sys
+from src.components import data_ingestion,data_transformation,model_trainer
+from src.exception import CustomException 
+from src.logger import logging
+from src.utils.utils import MainUtils
+from src.constant import *
+
+class TrainingPipeline:
+
+    def start_data_ingestion(self):
+        try:
+            data_ingestion = DataIngestion()
+            raw_data_dir = data_ingestion.initiate_data_ingestion()
+            return raw_data_dir
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def start_data_transformation(self,raw_data_dir):
+        try:
+            data_transformation = DataTransformation(raw_data_dir)
+            train_arr,test_arr,preprocessor_path = data_transformation.initiate_data_transformation()
+            return train_arr,test_arr,preprocessor_path
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def start_model_training(self,train_arr, test_arr,preprocessor_path):
+        try:
+            model_trainer = ModelTrainer()
+            model_score = model_trainer.initiate_model_trainer(train_arr,test_arr,preprocessor_path)
+            return model_score
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def run_pipeline(self):
+        try:
+            raw_data_dir = self.start_data_ingestion()
+            train_arr,test_arr,preprocessor_path = self.start_data_transformation(raw_data_dir)
+            r2_square = self.start_model_training(train_arr,test_arr,preprocessor_path)
+            print("Training Completed \n Trained Model Score : {}".format(r2_square))
+
+        except Exception as e:
+            raise CustomException(e,sys)
